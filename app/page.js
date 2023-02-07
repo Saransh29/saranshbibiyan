b464f66c53c264df02694a01cb7a2477941779ce
Saransh29 <saranshbibiyan234@gmail.com>
2023-02-07 22:59:40 +0530

    added About section

--- app/page.js
+++ app/page.js
@@ -1,8 +1,10 @@
 import Content from "@/components/Content";
+import AboutSection from "@/components/AboutSection";
 export default function Home() {
   return (
     <main className="mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl">
       <Content />
+      <AboutSection />
     </main>
   );
 }
