cd41887577cc60c280d18ea9d2c8bec6b56e02d9
Saransh29 <saranshbibiyan234@gmail.com>
2023-02-07 21:21:49 +0530

    added responsive navbar for mobile

--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+import React from "react";
 export default function Home() {
   return <h1 className="text-3xl font-bold underline"></h1>;
 }
--- components/navbar.js
+++ components/navbar.js
@@ -1,7 +1,24 @@
 "use client";
 import React, { useState } from "react";
+import { Link } from "react-scroll/modules";
 import { useTheme } from "next-themes";
 import { RiMoonFill, RiSunLine } from "react-icons/ri";
+import { IoMdMenu, IoMdClose } from "react-icons/io";
+
+const NAV_ITEMS = [
+  {
+    label: "Home",
+    page: "home",
+  },
+  {
+    label: "About",
+    page: "about",
+  },
+  {
+    label: "Projects",
+    page: "projects",
+  },
+];
 
 export default function Navbar() {
   const { systemTheme, theme, setTheme } = useTheme();
@@ -9,32 +26,69 @@ export default function Navbar() {
   const [navbar, setNavbar] = useState(false);
 
   return (
-    <header className="w-full mx-auto px-4 bg-white shadow fixed top-0 z-50 dark:bg-stone-900 dark:border-b dark:border-stone-600">
+    <header className="w-full mx-auto  px-4 sm:px-20 fixed top-0 z-50  bg-transparent shadow dark:bg-black dark:border-b dark:border-black">
       <div className="justify-between md:items-center md:flex">
         <div>
-          <div className="md:py-5 md:block">
-            <h2 className="text-4xl font-bold">Saransh Bibiyan</h2>
+          <div className="flex items-center justify-between py-3 md:py-5 md:block">
+            <div className="md:py-5 md:block">
+              <h2 className="text-4xl font-bold text-neutral-900 dark:text-neutral-100">
+                Saransh Bibiyan
+              </h2>
+            </div>
+            <div className="md:hidden">
+              <button
+                onClick={() => {
+                  setNavbar(!navbar);
+                }}
+              >
+                {navbar ? <IoMdClose size={30} /> : <IoMdMenu size={30} />}
+              </button>
+            </div>
           </div>
         </div>
-        <div className="md:flex md:space-x-6">
-          <a>About</a>
-          <a>Projects</a>
-          <a>Contact </a>
-          {currentTheme === "dark" ? (
-            <button
-              onClick={() => setTheme("light")}
-              className="bg-slate-100 p-2 rounded-xl"
-            >
-              <RiSunLine size={25} color="black" />
-            </button>
-          ) : (
-            <button
-              onClick={() => setTheme("dark")}
-              className="bg-slate-100 p-2 rounded-xl"
-            >
-              <RiMoonFill size={25} />
-            </button>
-          )}
+        <div>
+          <div
+            className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0  ${
+              navbar ? "block" : "hidden"
+            }`}
+          >
+            <div className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
+              {NAV_ITEMS.map((item, idx) => {
+                return (
+                  <Link
+                    key={idx}
+                    to={item.page}
+                    className={
+                      "block lg:inline-block text-neutral-900  hover:text-neutral-500 dark:text-neutral-100"
+                    }
+                    activeClass="active"
+                    spy={true}
+                    smooth={true}
+                    offset={-100}
+                    duration={500}
+                    onClick={() => setNavbar(!navbar)}
+                  >
+                    {item.label}
+                  </Link>
+                );
+              })}
+              {currentTheme === "dark" ? (
+                <button
+                  onClick={() => setTheme("light")}
+                  className="bg-slate-100 p-2 rounded-xl"
+                >
+                  <RiSunLine size={25} color="black" />
+                </button>
+              ) : (
+                <button
+                  onClick={() => setTheme("dark")}
+                  className="bg-slate-100 p-2 rounded-xl"
+                >
+                  <RiMoonFill size={25} />
+                </button>
+              )}
+            </div>
+          </div>
         </div>
       </div>
     </header>
