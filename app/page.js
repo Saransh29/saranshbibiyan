2e279e3d0c6722ec90621bf0be41ba71df3e4fbe
Saransh29 <saranshbibiyan234@gmail.com>
2023-02-08 22:46:30 +0530

    added analytics

--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@ import Content from "@/components/Content";
 import AboutSection from "@/components/AboutSection";
 import Projects from "@/components/Projects";
 import Footer from "@/components/footer";
+import { Analytics } from "@vercel/analytics/react";
 export default function Home() {
   return (
     <main className="mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl">
@@ -9,6 +10,7 @@ export default function Home() {
       <AboutSection />
       <Projects />
       <Footer />
+      <Analytics />
     </main>
   );
 }
