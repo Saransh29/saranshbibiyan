43f5c6ccd90d19d76cfa753eaff9464159c5883f
Saransh29 <saranshbibiyan234@gmail.com>
2023-02-11 22:11:15 +0530

    added og-image edge config from api

--- app/head.js
+++ app/head.js
@@ -8,6 +8,10 @@ export default function Head() {
         content="Hi, I'm Saransh Bibiyan a Web Developer based in Delhi, India. Currently pursuing my final year B.Tech. from
           NSUT, Delhi & building web apps with React and Next.js."
       />
+      <meta
+        property="og:image"
+        content="https://www.saransh.me/api/og?title=SARANSH%20BIBIYAN"
+      />
       <link rel="icon" href="/favicon.ico" />
     </>
   );
---
+++ pages/api/og.js
@@ -0,0 +1,66 @@
+import { ImageResponse } from "@vercel/og";
+import { NextRequest } from "next/server";
+
+export const config = {
+  runtime: "edge",
+};
+
+const font = fetch(
+  new URL("../../public/fonts/kaisei-tokumin-bold.ttf", import.meta.url)
+).then((res) => res.arrayBuffer());
+
+export default async function handler(req, res) {
+  const { searchParams } = req.nextUrl;
+  const postTitle = searchParams.get("title");
+  const fontData = await font;
+
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          height: "100%",
+          width: "100%",
+          display: "flex",
+          // flexDirection: "column",
+          // alignItems: "flex-start",
+          textAlign: "center",
+          justifyContent: "center",
+          alignItems: "center",
+
+          backgroundImage:
+            "linear-gradient(109.6deg, rgb(0, 0, 0) 11.2%, rgb(11, 132, 145) 91.1%)",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            padding: "0 20px",
+            textAlign: "center",
+            justifyContent: "center",
+            alignItems: "center",
+            fontSize: 100,
+            fontFamily: "Kaisei Tokumin",
+            letterSpacing: "-0.05em",
+            fontStyle: "normal",
+            color: "white",
+            lineHeight: "120px",
+            whiteSpace: "pre-wrap",
+          }}
+        >
+          {postTitle}
+        </div>
+      </div>
+    ),
+    {
+      width: 1200,
+      height: 630,
+      fonts: [
+        {
+          name: "Kaisei Tokumin",
+          data: fontData,
+          style: "normal",
+        },
+      ],
+    }
+  );
+}
