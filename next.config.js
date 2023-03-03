4ed1ccfdd52ec8e999513fd98d0c53cea023a074
Saransh29 <saranshbibiyan234@gmail.com>
2023-03-03 21:40:52 +0530

    added redirects and auth to paths

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  images: {
+    domains: ["avatars.githubusercontent.com","lh3.googleusercontent.com"],
+  },
   async redirects() {
     return [
       {
@@ -10,6 +13,21 @@ const nextConfig = {
         destination: "/resume.pdf",
         permanent: true,
       },
+      {
+        source:"/github",
+        destination:"https://github.com/saransh29",
+        permanent:true,
+      },
+      {
+        source:"/linkedin",
+        destination:"https://www.linkedin.com/in/saransh-bibiyan/",
+        permanent:true,
+      },
+      {
+        source:"/lc",
+        destination:"https://leetcode.com/sky_sk/",
+        permanent:true,
+      }
     ];
   },
 };
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,13 +1,19 @@
-import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+
 
 export const authOptions = {
   providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
   ],
-}
+};
 
-export default NextAuth(authOptions)
+export default NextAuth(authOptions);
