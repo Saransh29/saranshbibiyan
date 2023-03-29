a32fc42254b2489c21d6eada18b9734d0c84fc1e
Saransh29 <saranshbibiyan234@gmail.com>
2023-03-30 02:00:13 +0530

    changed ai-builder image

--- components/Projects.js
+++ components/Projects.js
@@ -8,7 +8,7 @@ const projects = [
     name: "Ai Builder",
     description:
       "Develop full websites and web apps using Ai Builder. Ai Builder is a low code platform that allows you to build websites and web apps without writing a single line of code.",
-    image: "/ai-builder-1.png",
+    image: "/ai-build.png",
     tech: "React, Nextjs, OpenAI, MongoDB, Nodejs, Expressjs",
     github: "https://github.com/Saransh29/ai-builder",
     link: "https://ai-builder-gules.vercel.app/",
